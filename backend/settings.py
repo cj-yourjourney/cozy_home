5f00fd3894d8d5c742c2213ed09b6ae827d53549
JesonDerulo <jasonderulo110015@gmail.com>
2024-03-28 14:35:00 -0700

    testing

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
